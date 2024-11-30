f568dfdb59d912d1e636021a997dca375112fd8a
Marcos Flavio <mfghost69@gmail.com>
2024-11-30 14:09:08 -0300

    feat: file handling done

--- lib/video_streaming_poc_web/controllers/files/file_controller.ex
+++ lib/video_streaming_poc_web/controllers/files/file_controller.ex
@@ -1,13 +1,14 @@
 defmodule VideoStreamingPocWeb.Files.FileController do
   use VideoStreamingPocWeb, :controller
 
-  alias VideoStreamingPoc.Video
+  # alias VideoStreamingPoc.Video
 
-  def upload(conn, %{"video" => %Plug.Upload{} = video}) do
-    # Video.store(%Plug.Upload{})
+  def upload(conn, %{"video" => %Plug.Upload{} = file}) do
+    # Video.store(file)
+    IO.inspect file
 
     conn
     |> put_status(:ok)
-    |> json(%{message: "file uploaded", filename: video.filename})
+    |> json(%{message: "file uploaded", filename: file.filename})
   end
 end
