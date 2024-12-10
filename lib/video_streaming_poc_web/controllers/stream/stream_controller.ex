ba9979378e3cc1bc8c3895408e242e2a74cb2163
Marcos Flavio <mfghost69@gmail.com>
2024-12-10 12:27:58 -0300

    feat: stream content

--- lib/video_streaming_poc_web/controllers/stream/stream_controller.ex
+++ lib/video_streaming_poc_web/controllers/stream/stream_controller.ex
@@ -8,20 +8,58 @@ defmodule VideoStreamingPocWeb.Stream.StreamController do
 
   def stream(conn, %{"filename" => filename}) do
     {:ok, body} = Get.get_file(filename)
+    total_size = byte_size(body)
+    IO.inspect(total_size, label: "Total Size")
 
-    conn =
-      conn
-      |> Conn.put_resp_content_type("video/mp4")
-      |> Conn.put_resp_header("content-disposition", "inline; filename=\"#{filename}\"")
-      |> Conn.put_resp_header("accept-ranges", "bytes")
-      |> Conn.send_chunked(200)
+    case Conn.get_req_header(conn, "range") do
+      ["bytes=" <> range] ->
+        conn = handle_range_request(conn, range, body, total_size)
+        conn
 
-    # Stream the video file to the client
-    stream_data(conn, body)
+      _ ->
+        # Default to sending the whole content if no Range header is present
+        conn =
+          conn
+          |> Conn.put_resp_content_type("video/mp4")
+          |> Conn.put_resp_header("content-disposition", "inline; filename=\"#{filename}\"")
+          |> Conn.put_resp_header("accept-ranges", "bytes")
+          |> Conn.send_chunked(200)
+
+        stream_data(conn, body)
+    end
+  end
+
+  defp handle_range_request(conn, range, body, total_size) do
+    {start, finish} = parse_range(range, total_size)
+    length = finish - start + 1
+    partial_content = binary_part(body, start, length)
+
+    conn
+    |> Conn.put_resp_content_type("video/mp4")
+    |> Conn.put_resp_header("content-disposition", "inline; filename=\"video.mp4\"")
+    |> Conn.put_resp_header("accept-ranges", "bytes")
+    |> Conn.put_resp_header("content-range", "bytes #{start}-#{finish}/#{total_size}")
+    |> Conn.send_chunked(206)
+    |> stream_data(partial_content)
+  end
+
+  defp parse_range(range, total_size) do
+    [start_str, finish_str] =
+      range
+      |> String.split("-")
+      |> Enum.map(&String.trim/1)
+
+    start = String.to_integer(start_str)
+    finish =
+      case finish_str do
+        "" -> total_size - 1
+        _ -> String.to_integer(finish_str)
+      end
+
+    {start, finish}
   end
 
   defp stream_data(conn, data) do
-    # 64 KB chunks
     chunk_size = 65_536
 
     data
@@ -34,7 +72,6 @@ defmodule VideoStreamingPocWeb.Stream.StreamController do
     end)
   end
 
-  # This function chunks the binary data
   defp chunk_binary(binary, chunk_size) do
     Stream.unfold(binary, fn
       <<chunk::binary-size(chunk_size), rest::binary>> -> {chunk, rest}
