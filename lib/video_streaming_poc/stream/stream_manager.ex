b7fb958258fbfe2512cc66b70a31b975404a8b91
Marcos Flavio <mfghost69@gmail.com>
2024-12-19 11:45:54 -0300

    style: run mix format

--- lib/video_streaming_poc/stream/stream_manager.ex
+++ lib/video_streaming_poc/stream/stream_manager.ex
@@ -2,17 +2,17 @@ defmodule VideoStreamingPoc.Stream.StreamManager do
   @moduledoc false
 
   def save_index(body, stream_key) do
-  path = Path.join(["priv/static/streams", stream_key, "index.m3u8"])
+    path = Path.join(["priv/static/streams", stream_key, "index.m3u8"])
 
-	File.mkdir_p!(Path.dirname(path))
-	File.write!(path, body)
+    File.mkdir_p!(Path.dirname(path))
+    File.write!(path, body)
 
-  IO.puts("Index.m3u8 saved in /streams/#{stream_key}")
-  {:ok, "Index saved"}
+    IO.puts("Index.m3u8 saved in /streams/#{stream_key}")
+    {:ok, "Index saved"}
   end
 
   def save_ts(body, stream_key, file_name) do
-  	path = Path.join(["priv/static/streams", stream_key, file_name])
+    path = Path.join(["priv/static/streams", stream_key, file_name])
 
     File.mkdir_p!(Path.dirname(path))
     File.write!(path, body)
